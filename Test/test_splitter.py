cf4693bdfd5d7f222e2309a587b5c8d89a00fc3a
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2024-12-12 21:44:26 +0000

    Added a function to find the index page

--- Test/test_splitter.py
+++ Test/test_splitter.py
@@ -6,7 +6,6 @@ import pytest
 from pypdf import PdfReader
 import splitter
 
-
 def test_get_file_exists():
     path_file = "test-pdfs/SE.pdf"
     reader = splitter.get_file(path_file)
@@ -16,3 +15,15 @@ def test_get_file_nonexistant():
     path_file = "test-pdfs/AI.pdf"
     with pytest.raises(UnboundLocalError):
         splitter.get_file(path_file)
+
+def test_find_index_page_ds():
+    path_file = "test-pdfs/DS.pdf"
+    reader = splitter.get_file(path_file)
+    page = splitter.find_index_page(reader)
+    assert page == 6
+
+def test_find_index_page_se():
+    path_file = "test-pdfs/SE.pdf"
+    reader = splitter.get_file(path_file)
+    page = splitter.find_index_page(reader)
+    assert page == 12
--- splitter.py
+++ splitter.py
@@ -8,5 +8,25 @@ def get_file(file_path):
         print(f"File at {file_path} not found")
     except:
         print("Unknown error")
-    
     return reader
+
+def find_index_page(reader):
+    keywords = [
+        'contents',
+        'index'
+        'table of contents'
+    ]
+
+    found = False
+    n = 0
+    while not found and n < len(reader.pages):
+        page = reader.pages[n]
+        text = page.extract_text()
+        text = text.lower()
+
+        for keyword in keywords:
+            if keyword in text:
+                found = True
+                return n + 1
+        n += 1
+    return None
