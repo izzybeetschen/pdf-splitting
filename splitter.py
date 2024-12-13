acf7831c79d5ba4fb1ee71ae886f1b3342e0962e
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2024-12-13 11:18:17 +0000

    Comments

--- splitter.py
+++ splitter.py
@@ -3,6 +3,18 @@ import os
 import re
 
 def get_file(file_path):
+    """
+    Gets the given file
+
+    Args:
+        file_path (Str): path to the given file
+    
+    Returns:
+        PdfReader: a variable containing the file 
+
+    Raises:
+        UnboundLocalError: raised if the file is not found
+    """
     try:
         reader = PdfReader(file_path)
     except UnboundLocalError:
@@ -12,6 +24,17 @@ def get_file(file_path):
     return reader
 
 def find_index_page(reader):
+    """
+    Finds the contents page of the textbook
+
+    Args:
+        reader (PdfReader): a variable containing the textbook file
+    
+    Returns:
+        int or None: 
+            returns the page number containing the contents page or None if no contents page is found
+
+    """
     keywords = [
         'contents',
         'table of contents'
@@ -22,11 +45,11 @@ def find_index_page(reader):
     while n < len(reader.pages):
         page = reader.pages[n]
         text = page.extract_text()
-        text = text.lower()
+        text = text.lower()     # sets text to lowercase
 
         for keyword in keywords:
             if keyword in text:
-                pattern = r"\w+[\s\.\-]+\d+"
+                pattern = r"\w+[\s\.\-]+\d+"    # checks pattern to ensure contents page
                 if re.search(pattern, text):
                     return n + 1
 
