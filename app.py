e1d7eee387073b95aa0b1910624b6f124a200779
Izzy Beetschen <114227602+izzybeetschen@users.noreply.github.com>
2024-12-16 21:56:26 +0000

    Fixed stupid errors from html... need to restyle now :(

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-# Explicitly set debug mode in the app config
-app.config['DEBUG'] = True
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
